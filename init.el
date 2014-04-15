8ace11fa99df57288aaf818542cc5e227a832b28
chia-chun <chia-chun@users.noreply.github.com>
2014-04-15 20:10:54 +0800

    paren

--- init.el
+++ init.el
@@ -5,8 +5,10 @@
 (require 'whitespace)
 (setq whitespace-line-column 79)
 (setq whitespace-style '(face empty tabs lines-tail trailing))
-(global-whitespace-mode t) 
+(global-whitespace-mode t)
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
 (set-face-attribute 'font-lock-comment-face nil :foreground "#3f7f5f")
+
+(show-paren-mode 1)
