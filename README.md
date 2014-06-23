8053f9baf216848cc2bea703046a4b1b9a8576e6
chia-chun <chia-chun@users.noreply.github.com>
2014-06-23 18:12:26 +0800

    fci

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
 I like the power of emacs but also think the text editing
 function of vi/vim is better.
 So I decided to use emacs + EVIL.
-
-2013 Aug 19
-Change the comment color.
-Add the following to init.el
-
-(set-face-attribute 'font-lock-comment-face nil :foreground "#3f7f5f")
-
-2013 Aug 15
-Add whitespace and 79 column visualization.
-Add auto cleanup.
-Add the following to init.el
-
-(require 'whitespace)
-(setq whitespace-line-column 79)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
-(global-whitespace-mode t)
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
-2013 Aug 9
-Fixed the problem of using TAB in org mode.
-Remove the following binding in evil-maps.el
-
-;(when evil-want-C-i-jump
-;  (define-key evil-motion-state-map (kbd "C-i") 'evil-jump-forward))
--- init.el
+++ init.el
@@ -1,10 +1,10 @@
+(add-to-list 'load-path "~/.emacs.d/fci")
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
 (require 'whitespace)
-(setq whitespace-line-column 79)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-style '(face empty tabs trailing indentation::space))
 (global-whitespace-mode t)
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
@@ -12,3 +12,16 @@
 (set-face-attribute 'font-lock-comment-face nil :foreground "#3f7f5f")
 
 (show-paren-mode 1)
+
+(require 'fill-column-indicator)
+(setq-default fci-rule-column 80)
+(setq fci-handle-truncate-lines nil)
+(define-globalized-minor-mode global-fci-mode fci-mode
+  (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+(defun auto-fci-mode (&optional unused)
+  (if (> (window-width) (+ fci-rule-column 1))
+      (fci-mode 1)
+      (fci-mode 0)))
+(add-hook 'after-change-major-mode-hook 'auto-fci-mode)
+(add-hook 'window-configuration-change-hook 'auto-fci-mode)
